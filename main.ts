e15a167770b48f851ccdd9a60aa7191203155444
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-10-28 18:34:36 +0000

    simplified comparison

--- main.ts
+++ main.ts
@@ -1,53 +1,53 @@
 radio.onReceivedStringDeprecated(function (receivedString) {
     item = receivedString
-    if (item.compare("A") == 0) {
+    if (item == "A") {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
         -200,
         SuperBit.enMotors.M3,
         -200
         )
-    } else if (item.compare("B") == 0) {
+    } else if (item == "B") {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
         200,
         SuperBit.enMotors.M3,
         200
         )
-    } else if (item.compare("C") == 0) {
+    } else if (item == "C") {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
         200,
         SuperBit.enMotors.M3,
         -200
         )
-    } else if (item.compare("S") == 0) {
+    } else if (item == "S") {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
         0,
         SuperBit.enMotors.M3,
         0
         )
-    } else if (item.compare("0") == 0) {
+    } else if (item == "0") {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
         0,
         SuperBit.enMotors.M3,
         0
         )
-    } else if (item.compare("E") == 0) {
+    } else if (item == "E") {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Red))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("F") == 0) {
+    } else if (item == "F") {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Green))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("G") == 0) {
+    } else if (item == "G") {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Blue))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("H") == 0) {
+    } else if (item == "H") {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Yellow))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("I") == 0) {
+    } else if (item == "I") {
         SuperBit.RGB_Program().clear()
         SuperBit.RGB_Program().show()
     }
@@ -55,6 +55,7 @@ radio.onReceivedStringDeprecated(function (receivedString) {
 let item = ""
 radio.setGroup(1)
 radio.setTransmitPower(7)
+item = "S"
 basic.showLeds(`
     . . . . .
     . # . # .
